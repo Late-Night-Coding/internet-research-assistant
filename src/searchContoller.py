0bf630d62faec1069ad7c1b0a704373e475e29d2
Peter Pham <ptrpham2468@gmail.com>
2023-04-05 14:40:23 -0500

    enhanced the summary and made the searches async

--- src/searchContoller.py
+++ src/searchContoller.py
@@ -7,13 +7,16 @@ import flask
 
 class SearchController:
     def __init__(self):
-        self.bingSearcher = Bing()
-        response = self.bingSearcher.search("orthogonal vectors")
+        # query = "iasubyaseiruntgiuaydhfbi auhergi uhaedfkbn wiertuh skdjhn wseirtuyh df "
+        # self.bingSearcher = Bing()
+        # response = self.bingSearcher.search(query)
+        # print(response)
+        #
+        # response = asyncio.run(google.search(query, 10, 10, 10))
+        # print(response)
+        openai = OpenAI()
+        response = openai.chat("what are some categories for the following phrase: Pokemon")
         print(response)
-
-        response = asyncio.run(google.search('orthogonal vectors', 10, 10, 10))
-        print(response)
-
 
 if __name__ == "__main__":
     main = SearchController()
--- src/searches/openai_search.py
+++ src/searches/openai_search.py
@@ -1,55 +1,52 @@
 import json
-import openai
+import aiohttp
+import asyncio
 
 
 class OpenAI:
     def __init__(self):
         self.key = "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"
-        openai.organization = "org-nVcfRvKHlZzuZmUk3kTRiXMP"
-        openai.api_key = self.key
+        self.organization = "org-nVcfRvKHlZzuZmUk3kTRiXMP"
+        self.endpoint = "https://api.openai.com/v1/"
 
-    def categorize(self, prompt):
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt="what are some categories for the following phrase: " + prompt,
-            temperature=0.1,
-            max_tokens=1000,
-            top_p=1,
-            best_of=3,
-            frequency_penalty=0,
-            presence_penalty=1.1
-        )
+    async def _request(self, endpoint, data):
+        async with aiohttp.ClientSession() as session:
+            headers = {"Authorization": f"Bearer {self.key}"}
+            async with session.post(self.endpoint + endpoint, headers=headers, json=data) as resp:
+                return await resp.json()
 
-        return self.__format__(response)
-
-    def summarize(self, prompt):
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt="summarize this: " + prompt,
-            temperature=0.1,
-            max_tokens=1000,
-            top_p=1,
-            best_of=3,
-            frequency_penalty=0,
-            presence_penalty=1.1
-        )
+    async def categorize(self, prompt):
+        response = await self._request("completions", {
+            "model": "text-davinci-003",
+            "prompt": f"what are some categories for the following phrase: {prompt}",
+            "temperature": 0.1,
+            "max_tokens": 1000,
+            "top_p": 1,
+            "best_of": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 1.1,
+        })
 
         return self.__format__(response)
 
+    async def summarize(self, prompt):
+        response = await self._request("completions", {
+            "model": "text-davinci-003",
+            "prompt": "summarize this: " + prompt,
+            "temperature": 0.1,
+            "max_tokens": 1000,
+            "top_p": 1,
+            "best_of": 2,
+            "frequency_penalty": 0,
+            "presence_penalty": 1.1,
+        })
+
+        return self.__format__(response)
 
     def __format__(self, response):
-        data = json.loads(str(response))  # convert the response to a string and then load it as a json variable
-
-        response = data["choices"][0]["text"]  # only get the response
-
-        response = response.strip()  # strip all leading and trailing lines and spaces
-        response = response.split("\n")  # split it to make a list
+        response = response["choices"][0]["text"].strip().split("\n")
 
         if len(response) == 1:
-            return response[0]  # return if the response is not a list
+            return response[0]
 
-        return [elem.split('.q ')[1] for elem in response]  # remove the leading numbers and return it
-
-# if __name__ == "__main__":
-#     x = OpenAI()
-#     print(x.chat("what are some related topics to the sentence 'dogs':"))
+        return [elem.split('.q ')[1] for elem in response]
