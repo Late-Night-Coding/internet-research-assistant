558158e1cad60212ea58221fd3554f0c0902af79
tronixtus <tjc190001@utdallas.edu>
2023-04-06 15:32:02 -0500

    fixed test format

--- src/frontend/test/browserTest.py
+++ src/frontend/test/browserTest.py
@@ -1,14 +1,24 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# TODO find component names
-driver = webdriver.Chrome('./chromedriver')
-driver.get("localhost:8080")
-search_bar = driver.find_element_by_name("searchbar")
-search_bar.clear()
-search_bar.send_keys("baseball")
-search_bar.send_keys(Keys.RETURN)
+class BrowserTest(unittest.TestCase):
 
-# TODO: figure out what to assert
-assert()
-driver.close()
+    driver:webdriver
+    def setUp(self):
+        self.driver = webdriver.Chrome('./chromedriver')
+ 
+    
+    def testHasResults(self):
+        driver = self.driver
+        driver.get("localhost:8080")
+        search_bar = driver.find_element_by_name("searchbar")
+        search_bar.clear()
+        search_bar.send_keys("baseball")
+        search_bar.send_keys(Keys.RETURN)
+
+        assert driver.find_element_by_name("searchResults") is not 0
+ 
+    def tearDown(self):
+        self.driver.close()
+    
