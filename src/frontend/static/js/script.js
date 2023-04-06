a17989037338a496aa3ebaf18b14bd7d0b7e6df8
blake <blake>
2023-04-06 15:51:17 -0500

    Add comments & minor refactors to javascript

--- src/frontend/static/js/script.js
+++ src/frontend/static/js/script.js
@@ -7,22 +7,23 @@ const searchInput = searchForm.querySelector('input[name="query"]');
 
 // Function to perform lookup search
 function performSearch(query) {
-  
   searchInput.value = query; // set the value of the search input to the query
   searchForm.submit();
 }
 
+// global vars to keep track of menu
 let menuShown = false;
 const scopes = document.querySelectorAll(".searchResult");
 var menu = document.querySelector(".menu");
-let selectedText = "";
 
+// Display menu when user selects text in a search result
 for(s of scopes){
     s.addEventListener("mouseup", () => {
 
         let selection = document.getSelection();
         let selectedText = selection.toString();
 
+        // if text is selected, validate the selection and show the menu
         if (selectedText.length > 0) {
 
             if(selectedText.charCodeAt(0) < 31){
@@ -30,30 +31,19 @@ for(s of scopes){
                 if(selectedText.charCodeAt(0) < 31){
                     menu.style.display = "none";
                 }
-
             } else{
                 menu.style.display = "block";
                 updateMenuLocation();    
-
-                var lookupbtn = document.getElementById("lookup-btn");
-        
-                document.addEventListener("click", function(event){
-    
-                    if(event.target == lookupbtn){
-                        performSearch(currInputQuery + ' ' + selectedText);
-                        menu.style.display = "none";
-                    } 
-                });
             }
-    
+
+        // if no text selected, hide menu
         } else {
             menu.style.display = "none";
-            console.log("Selected text length 0: " + selectedText);
         }
     });
-    
 }
 
+// When the user clicks to clear the selection, hide the menu
 window.addEventListener("mouseup", (e) => {
 
     let selectedText = window.getSelection().toString();
@@ -61,9 +51,10 @@ window.addEventListener("mouseup", (e) => {
     if(selectedText == ""){
         menu.style.display = "none";
         window.getSelection().removeAllRanges();
-    }
+    } 
 });
 
+// Set the menu's position to the position of the selection
 function updateMenuLocation(){
     const range = window.getSelection().getRangeAt(0);
     const rect = range.getBoundingClientRect();
@@ -71,12 +62,13 @@ function updateMenuLocation(){
     menu.style.left = rect.left + window.pageXOffset + 'px';
 }
 
+// Update the menu position when the window is resized
 window.addEventListener("resize", function(){
     updateMenuLocation();
 });
 
+// Info icon
 const circleInfoIcon = document.querySelector('#info-icon');
-
 circleInfoIcon.addEventListener('click', function() {
     console.log("icon clicked");
 });
