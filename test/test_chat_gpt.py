7405ae772333ed369bf31f94469e10786f7202e0
blake <blake>
2023-04-11 04:18:55 -0500

    OpenAI refactor & tests

--- test/test_chat_gpt.py
+++ test/test_chat_gpt.py
@@ -1,8 +1,42 @@
 import asyncio
-import pytest
+from data.search_history import SearchHistory
+from openai_api import OpenAI
 
-# TODO: Make chatgpt async
-def test_query():
-    raise "not implemented"
+def test_summarize():
+    open_ai = OpenAI()
+    dog_wiki_summary = """The dog (Canis familiaris[4][5] or Canis lupus familiaris[5]) is a domesticated descendant of the wolf.
+        Also called the domestic dog, it is derived from the extinct Pleistocene wolf,[6][7] and the modern wolf is the dog's nearest living relative.[8]
+        Dogs were the first species to be domesticated[9][8] by hunter-gatherers over 15,000 years ago[7] before the development of agriculture."""
+    dog_description = asyncio.run(open_ai.summarize("dog", dog_wiki_summary))
 
+    assert isinstance(dog_description, str)
+    assert len(dog_description) > 0
+    
+    print(f"Dear tester, please validate this description of 'dog':\n{dog_description}")
 
+def test_non_recursive_get_search_terms():
+    open_ai = OpenAI()
+    search_history = SearchHistory("animal", "id1")
+
+    search_terms = asyncio.run(open_ai.get_search_keywords(search_history))
+
+    assert isinstance(search_terms, list)
+    assert len(search_terms) > 1
+    assert isinstance(search_terms[0], str)
+    
+    print(f"Dear tester, please validate this set of search terms for 'animal':\n{str(search_terms)}")
+
+def test_recursive_get_search_terms():
+    open_ai = OpenAI()
+    parent_topic = 'baseball'
+    child_topic = 'teams'
+    search_history = SearchHistory(parent_topic, "id1")
+    search_history.append_keyword(child_topic)
+
+    search_terms = asyncio.run(open_ai.get_search_keywords(search_history))
+
+    assert isinstance(search_terms, list)
+    assert len(search_terms) > 1
+    assert isinstance(search_terms[0], str)
+    
+    print(f"Dear tester, please validate this set of search terms for '{parent_topic} > {child_topic}':\n{str(search_terms)}")
